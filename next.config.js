e7b7e2df7c062f520eb71fbda763a7a47b1d233c
PerTrakanjan <138445215+PerTrakanjan@users.noreply.github.com>
2025-03-29 16:23:52 +0700

    Merge Branch Develop To Main (#3)
    * Add: ui and skeleton
    
    * Update: fetching data from db of club
    
    * Add : link to club detail page
    
    * add: search function
    
    * add: filter by FilterSelection
    
    * add club-sctivities skeleton and UI
    
    * add: testing
    
    * Implemented activity detail page with dynamic content
    
    * Add search & sort functionality, improve activity details display
    
    * add testing
    
    * Add: Feature Details Clubs (Skeleton Include)
    
    * Update select component
    
    * Add club detail with activities page , mock more data,responsive
    
    * Add: API GET Get Club By ID
    
    * add favorite clubs page
    
    * Refactor: Use API Mockup & Edit All Page Call API
    
    * Fix: Skeleton Not Show in President, Advisor, Details Club
    
    * add club-sctivities skeleton and UI
    
    * Implemented activity detail page with dynamic content
    
    * Add search & sort functionality, improve activity details display
    
    * add testing
    
    * Update select component
    
    * Add: Feature Details Clubs (Skeleton Include)
    
    * Add club detail with activities page , mock more data,responsive
    
    * Add: API GET Get Club By ID
    
    * Refactor: Use API Mockup & Edit All Page Call API
    
    * add: create api for club
    
    * Add: Committee in Club Details
    
    * edit: club type
    
    * add: club test
    
    * add favorite clubs functionality
    
    * Fix activity card display to show title and description from API
    
    * Implement API fetching for activity details
    
    * Fix activity loading and add fall back data
    
    * add: testing code with all story
    
    * fix a wrong text
    
    * Add APi endpoints for fetching past and ongoing activities,fix sorting and search
    
    * fix: add skeleton
    
    * fix: maintain loading test
    
    * Add: UAT Testing Featue Club Details
    
    * Delete c:\Users\User\Documents\test_club\club-detail.png
    
    * fix testing
    
    * Add: UAT Testing FeatureClub Activities
    
    * remove window reload in home page
    
    * Squashed commit of the following:
    
    commit 5a77e777e1af54a8ad82af5c3a702d5343083528
    Author: PerTrakanjan <thanakrit.tra@ku.th>
    Date:   Thu Mar 27 02:36:54 2025 +0700
    
        add tests for favorite clubs functionality
    
    commit 94b870cce4aa9cd1312910ab6e7067d16d806d6f
    Author: PerTrakanjan <thanakrit.tra@ku.th>
    Date:   Thu Mar 27 02:36:44 2025 +0700
    
        refactor club fetching with search and sorting parameters
    
    commit f0283bae9ba4137d8e44cde814afd323649ffe85
    Author: PerTrakanjan <thanakrit.tra@ku.th>
    Date:   Thu Mar 27 02:35:47 2025 +0700
    
        add handler prop to ClubBox
    
    commit ef72f47cc33fff4689bae1285d9cb49b53537408
    Author: PerTrakanjan <thanakrit.tra@ku.th>
    Date:   Thu Mar 27 02:35:01 2025 +0700
    
        add sorting  to club search results
    
    commit 48dc838b31b1cf776383ed06c94f71eea671fe70
    Author: PerTrakanjan <thanakrit.tra@ku.th>
    Date:   Wed Mar 26 13:07:35 2025 +0700
    
        add search and sorting to favorite clubs page
    
    * add GitHub Actions workflow for pull request testing
    
    * update pull request workflow to trigger on develop and feature branches
    
    * add Dockerfile
    
    * configure Next.js
    
    * add CI workflow for Docker development environment
    
    * update test command
    
    * remove test execution step f
    
    * add Docker production workflow
    
    * update club page test to expect 37 skeletons
    
    ---------
    
    Co-authored-by: Nut <vimudakorn.k@gmail.com>
    Co-authored-by: imyourjxpxn <yeepoon.ps@gmail.com>
    Co-authored-by: Natdanai Pinaves <jgogo012@gmail.com>
    Co-authored-by: Natdanai Pinaves <83464496+jgogo01@users.noreply.github.com>
    Co-authored-by: Perthanakrit <aupper094@gmail.com>

---
+++ next.config.js
@@ -0,0 +1,25 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'images.unsplash.com',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'source.unsplash.com',
+        port: '',
+        pathname: '/**',
+      }
+    ],
+  },
+
+  output: 'standalone',
+  productionBrowserSourceMaps: false
+}
+
+module.exports = nextConfig
